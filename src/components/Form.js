7a4d95904636d41af76452adff4122141905d2ee
Job koech <jobkoech610@gmail.com>
2023-04-21 19:25:28 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedFirstName, setSubmittedFirstName] = useState("");
+  const [submittedLastName, setSubmittedLastName] = useState("");
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,11 +14,29 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = {
+      firstName: firstName,
+      lastName: lastName,
+    };
+    setSubmittedFirstName(formData.firstName);
+    setSubmittedLastName(formData.lastName);
+    setFirstName("");
+    setLastName("");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
+      {submittedFirstName && submittedLastName && (
+        <div>
+          <p>Submitted first name: {submittedFirstName}</p>
+          <p>Submitted last name: {submittedLastName}</p>
+        </div>
+      )}
     </form>
   );
 }
